cae1692f8906e1f7a58397e03d0854a81dd691a9
Тимченко Евгений Михайлович <johny777@npopm.ru>
2014-12-01 22:20:32 +0700

    добавлен тестовый xml для разбора

---
+++ ora_xml/db/testxml.sql
@@ -0,0 +1,25 @@
+begin
+  admdba.skldok_pkg.SAVE_DOK(XMLtype('<doc type="Ã" wid="46">
+     <zag>
+          <nz>12345</nz>
+          <d_dok>11.11.2014</d_dok>
+          <post>80.09</post>
+          <potr>80.06</potr>
+     </zag>
+     <sod rowcount="2">
+          <row n="1">
+               <kod>ﬂﬂ123</kod>
+               <r_sort>Õ≈“</r_sort>
+               <kol_treb>1</kol_treb>
+               <prim></prim>
+          </row>
+          <row n="2">
+               <kod>ﬂﬂ123</kod>
+               <r_sort>Õ≈“</r_sort>
+               <kol_treb>1</kol_treb>
+               <prim></prim>
+          </row>
+     </sod>
+</doc>
+'));
+end;
